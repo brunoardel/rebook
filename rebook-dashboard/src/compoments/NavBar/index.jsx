7489d4f7aec57836be6a40e7b7e0e8c9cd83354d
Bruno Ardel <djbrunohardel@hotmail.com>
2020-02-01 11:17:02 -0300

    Logo na NavBar

--- rebook-dashboard/src/compoments/NavBar/index.jsx
+++ rebook-dashboard/src/compoments/NavBar/index.jsx
@@ -7,7 +7,11 @@ export default function NavBar() {
   return (
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Rebook</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <a href="#home">
+            <img src='https://www.reebokstudio.com.br/images/logo-footer.png' alt="Logo" width="130px" responsive/>
+          </a>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
