0cf2f476d28da6d3a887c4f8e1279976821acc90
Bruno Ardel <djbrunohardel@hotmail.com>
2020-02-01 20:23:32 -0300

    Cards - Correção das bordas

--- rebook-dashboard/src/compoments/CardGrafico/CardGrafico.jsx
+++ rebook-dashboard/src/compoments/CardGrafico/CardGrafico.jsx
@@ -4,7 +4,7 @@ import Grafico from '../../assets/img/Grafico.jpg'
 
 export default function CardGrafico () {
     return (
-        <Card className="text-center">
+        <Card className="text-center border-0">
             <Card.Body>
                 <Image src={Grafico} height="105" width="auto" />
                 <Card.Title className="mt-2">Taxa de Conversão</Card.Title>
--- rebook-dashboard/src/compoments/Sidebar/Sidebar.jsx
+++ rebook-dashboard/src/compoments/Sidebar/Sidebar.jsx
@@ -4,20 +4,24 @@ import CardGestor from "../CardGestor/CardGestor";
 import CardGrafico from "../CardGrafico/CardGrafico"
 
 export default function Sidebar () {
-  return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Item>
-        <CardGestor />
-      </Nav.Item>
-      <Nav.Item>
-        <CardGrafico />
-      </Nav.Item>
-      <Nav.Item>
-        <CardGrafico />
-      </Nav.Item>
-      <Nav.Item>
-        <CardGrafico />
-      </Nav.Item>
-    </Nav>
-  );
+    return (
+        <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Item>
+                <CardGestor />
+            </Nav.Item>
+            <div className="card">
+                <h5 className="text-center mt-1">Meta Faturamento</h5>
+                <Nav.Item>
+                    <CardGrafico />
+                </Nav.Item>
+                <Nav.Item>
+                    <CardGrafico />
+                </Nav.Item>
+                <Nav.Item>
+                    <CardGrafico />
+                </Nav.Item>
+
+            </div>
+        </Nav>
+    );
 }
