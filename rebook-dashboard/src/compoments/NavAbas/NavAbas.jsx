ceb18e7b6f0eeee14236139b090f077fe1dbf3c5
Bruno Ardel <djbrunohardel@hotmail.com>
2020-02-01 18:22:34 -0300

    Abas formatadas e sem conteudo

--- rebook-dashboard/src/compoments/NavAbas/NavAbas.jsx
+++ rebook-dashboard/src/compoments/NavAbas/NavAbas.jsx
@@ -1,22 +1,41 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Tab } from "react-bootstrap";
 
-export default function NavBar () {
+export default function NavAbas () {
     return (
-        <Nav justify variant="tabs" defaultActiveKey="/home">
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Painel Gerencial</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Análise Pipeline</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-3">Análise Esforço</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-4">Painel Indicadores</Nav.Link>
-            </Nav.Item>
-           
-        </Nav>
+        <>
+            <Tab.Container id="abas" defaultActiveKey="link-1">
+                <Nav justify variant="tabs">
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Painel Gerencial</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Análise Pipeline</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-3">Análise Esforço</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-4">Painel Indicadores</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Tab.Container>
+
+
+            <Tab.Content>
+                <Tab.Pane eventKey="link-1">
+                    <h4>link-1</h4>
+                </Tab.Pane>
+                <Tab.Pane eventKey="link-2">
+                    <h4>link-2</h4>
+                </Tab.Pane>
+                <Tab.Pane eventKey="link-3">
+                    <h4>link-3</h4>
+                </Tab.Pane>
+                <Tab.Pane eventKey="link-4">
+                    <h4>link-4</h4>
+                </Tab.Pane>
+            </Tab.Content>
+        </>
     );
 }
