6b27907205da0f2807123edcb3ad550e8107869b
Bruno Ardel <djbrunohardel@hotmail.com>
2020-02-03 03:45:10 -0300

    Graficos

--- rebook-dashboard/src/compoments/Body/Body.jsx
+++ rebook-dashboard/src/compoments/Body/Body.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Row, Col } from 'react-bootstrap'
 import Sidebar from "../Sidebar/Sidebar";
 import NavAbas from '../NavAbas/NavAbas'
+import "../Body/Body.css"
 
 export default function Body () {
     return (
-        <div className="mt-3 justify-content-between">
+        <div className="mt-3">
             <Row className="mx-2">
                 <Col md="auto">
                     <Sidebar />
--- rebook-dashboard/src/compoments/CardGrafico/CardGrafico.jsx
+++ rebook-dashboard/src/compoments/CardGrafico/CardGrafico.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Gauge from 'react-svg-gauge';
 import { Card, Image } from 'react-bootstrap';
 import Grafico from '../../assets/img/Grafico.jpg'
 
-export default function CardGrafico () {
+/* export default function CardGrafico () {
     return (
         <Card className="text-center border-0">
             <Card.Body>
@@ -10,4 +11,19 @@ export default function CardGrafico () {
             </Card.Body>
         </Card>
     )
+} */
+
+
+
+
+export default class CardGrafico extends Component {
+	render() {
+		return (
+			<div className="mt-0 pt-0 mb-4 d-flex flex-column justify-content-center align-items-center">
+				<Gauge value={73} width={105} height={115} label="" color="green"/>
+			</div>
+            
+		);
+	}
 }
+
